32ef4d8d90e6ba872b34be6897fb017bf0cb96e4
sirageDb <ilovesemicolons@hotmail.com>
2021-06-19 14:05:20 +0200

    FEAT : maxLength validator

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,13 @@ import containNumber from "./validators/containNumber"
 import isEmail from "./validators/isEmail";
 import containerValidator from "./validators/containerValidator"
 import minLength from "./validators/minLength";
+import maxLength from "./validators/maxLength";
 
 //should contain 2 capitalLetters && 2 numbers
 const password = "AA00qscqsc";
+const v_maxLength = maxLength(password, 20);
 
+console.log(v_maxLength);
 
 
 
